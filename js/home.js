276e3364d6ba4dff391bdd14187efb3f91ed51c6
Justin Koster <200100@virtualwindow.co.za>
2021-10-06 17:05:22 +0200

    changed most of the layout of the home page
    i changed most of the layout of the home page, and added bootstrap functionality, i also started to implement the hamburger icon going to be used for the tablet and mobile displays

--- js/home.js
+++ js/home.js
@@ -1,19 +1,32 @@
-$(document).ready(function () {
+$(document).ready(function(){
+
+    //image slider
     $(document).ready(function(){
-		var x = 0;
+        var x = 0;
     // for next slide
-		$('.btn-next').click(function(){
+        $('.btn-next').click(function(){
     
         x= (x<=100)?(x+100):0;
       $('figure').css('left', -x+'%');
-		});
-
-
+        });
+    
+    
      // for prev slide
      $('.btn-prev').click(function(){
     
         x= (x>=100)?(x-100):200;
       $('figure').css('left', -x+'%');
     });
-	});
-});
+    });
+    
+    function myFunction() { 
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+    
+    })
+    
