0ac708f62cce95ba2835e5eb48a08bb1ff400972
Cara <21100136@virtualwindow.co.za>
2021-10-17 16:34:25 +0200

    individual jquery

--- js/individual.js
+++ js/individual.js
@@ -1,34 +1,42 @@
 $(function(){
 
-   // const queryString = window.location.search;
-  //  const urlParams = new URLSearchParams(queryString);
-   // const id = urlParams.get('id');
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
 
-    const url = 'https://api.themoviedb.org/3/movie/550?api_key=60d58f5803d2a10a9d7bc0427dbc5023' ;
+    const url = 'https://api.themoviedb.org/3/movie/550?api_key=60d58f5803d2a10a9d7bc0427dbc5023' + id;
 
     $.getJSON(url, function(result){
 
         console.log(result);
 
-        film_name = original_title;
-        launch_name = overview;
-        postrer = poster_path;
-        genre = genres.name;
-        rating = genres.name.id;
-        bgimg = backdrop_path;
-       studio = production_companies.name;
-       date = release_date;
-      duration = runtime;
+        film_name = result.original_title;
+        launch_name = result.overview;
+        postrer = result.poster_path;
+        genre = result.genres.name;
+        rating = result.genres.name.id;
+        bgimg = result.backdrop_path;
+       studio = result.production_companies.name;
+       date = result.release_date;
+      duration = result.runtime;
 
        
        
 
      
 
-        $(".title").text(film_name[1]);
+        $(".title").text(film_name);
+        $(".text-genre").text(date + genre );
+        $(".studio").text( studio );
+        $(".genre").text(duration );
+        $(".Age-rating").text( rating );
+
+        $(".success_fail").css(" background-image","bgimg");
+
+
       
 
-        //$(".patch").attr("src", launch_image[1]);
+        $(".Movie-picture").attr("src", postrer);
 
         if(film_desc[1]=== null){
             $(".descriptin").text("There where no details found for this film");
