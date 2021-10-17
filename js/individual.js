cde0875ec482f5805b63c862ccb5fc3d5a01717e
Cara <21100136@virtualwindow.co.za>
2021-10-17 14:38:49 +0200

    Update individual.js

--- js/individual.js
+++ js/individual.js
@@ -1,33 +1,40 @@
-$(document).ready(function () {
+$(function(){
 
-    const url = 'https://api.themoviedb.org/3/movie/550?api_key=60d58f5803d2a10a9d7bc0427dbc5023';
-  
-    $.getJSON(url, function (result) {
-      console.log(result);
-  
-      for (i = 0; i < result.length; i++) {
-  
+   // const queryString = window.location.search;
+  //  const urlParams = new URLSearchParams(queryString);
+   // const id = urlParams.get('id');
+
+    const url = 'https://api.themoviedb.org/3/movie/550?api_key=60d58f5803d2a10a9d7bc0427dbc5023' ;
+
+    $.getJSON(url, function(result){
+
+        console.log(result);
+
+        film_name = original_title;
+        launch_name = overview;
+        postrer = poster_path;
+        genre = genres.name;
+        rating = genres.name.id;
+        bgimg = backdrop_path;
+       studio = production_companies.name;
+       date = release_date;
+      duration = runtime;
+
+       
+       
+
+     
+
+        $(".title").text(film_name[1]);
       
-  
-      $(".item").append(
-  
-         "<div class='image'>\
-         </div>\
-        <h3>" + result[i].title + "</h3>\
-        <p>Description</p>\
-        <h5>0/10</h5>"
-      
-      
-    
-        );
-  
-  
-  
-  
-  
-      }
-  
+
+        //$(".patch").attr("src", launch_image[1]);
+
+        if(film_desc[1]=== null){
+            $(".descriptin").text("There where no details found for this film");
+        } else {
+            $(".descriptin").text ( film_desc);
+        }
+
     });
-  
-  
-  });
+});
