6e53760e06cf4fdf2a0e93b192e7b3abf595ae30
Justin Koster <200100@virtualwindow.co.za>
2021-10-18 00:21:30 +0200

    used API and refined response
    refined the responsiveness of the homepage aswell as made sure data can be appended properly to the home page for the movies, i aslo started to implement the nav bar burger icon for the tablet and mobile layouts of the site

--- JS/home.js
+++ JS/home.js
@@ -1,33 +1,47 @@
 
 $(document).ready(function () {
 
-  const url = 'https://api.themoviedb.org/3/movie/550?api_key=60d58f5803d2a10a9d7bc0427dbc5023';
+ const url = 'https://api.themoviedb.org/3/movie/popular?api_key=60d58f5803d2a10a9d7bc0427dbc5023&language=en-US&page=1'
+
+  
 
   $.getJSON(url, function (result) {
     console.log(result);
 
-    for (i = 0; i < result.length; i++) {
+    for (i = 0; i < 10; i++) {
 
-    
+      $(".header-home").append(
+        
 
-    $(".item").append(
 
-       "<div class='image'>\
-       </div>\
-      <h3>" + result[i].title + "</h3>\
-      <p>Description</p>\
-      <h5>0/10</h5>"
-    
-    
-  
       );
 
+      $(".movie").append(
 
+        "<div class='col-12 col-md-6 col-lg-3 card-container'>\
+            <div class='card'>\
+               <div class='img-top'>\
+               <img src='https://image.tmdb.org/t/p/original"+result.results[i].poster_path+"'class='card-img-top img-fluid' alt=''>\
+               </div>\
+               <div class='card-body'>\
+                  <h3>" + result.results[i].title + "</h3>\
+                  <div class='btn'><h4> Watch Now </h4>\
+                  </div>\
+                  <h2> " + result.results[i].vote_average + " </h2>\
+               </div>\
+            </div>\
+         </div>"
 
-
-
+      
+      ); 
     }
 
+  
+
+  });
+
+  $("burger").on(function(){
+    $(".overlay").css("display", "block")
   });
 
 
