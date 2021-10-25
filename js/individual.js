38eabfa87e87af91aa6e61089d443627f77a8b51
Cara <21100136@virtualwindow.co.za>
2021-10-25 16:46:01 +0200

    fixed pictures on individual

--- js/individual.js
+++ js/individual.js
@@ -17,11 +17,12 @@ $(function(){
 
          film_name =  result.original_title;
          film_desc =  result.overview;
-         postrer = result.poster_path;
+         postrer = "https://image.tmdb.org/t/p/original" + result.poster_path ;
           rating = result.vote_average;
           Prod = result.genres[0].name;
           ageRating =  result.adult;
-          bgimg = result.backdrop_path;
+          bgimg = "https://image.tmdb.org/t/p/original" + result.backdrop_path ;
+          // bgimg = ' https://image.tmdb.org/t/p/original"+ result.backdrop_path + " ';
           language = result.original_language;
           date =   result.release_date;
          studio = result.production_companies[0].name;
@@ -39,7 +40,7 @@ $(function(){
             $(".Age-rating").text ("16+" );
         }
 
-        //return (n + '').split('').map((i) => { return Number(i); })
+        
        
 
      console.log(bgimg)
@@ -50,9 +51,10 @@ $(function(){
         $(".Dur").text( studio);
    
 
-        $(".hover").css("background-image", "url(.." + bgimg + ")");
-        //$(".hover").css("background-image", "url( ../images/Venom.jpg )");
-         $(".Movie-picture").css("background-image", "url(" + bgimg + ")");
+        $(".hover").css("background-image", "url(" + bgimg + ")");
+       
+         $(".Movie-picture").css("background-image", "url(" + postrer + ")");
+         //$(".Movie-picture").attr("src",  postrer);
 
         
 
