ee18ce38f8749dc975a381f0a65b89c7baaea6fe
Justin Koster <200100@virtualwindow.co.za>
2021-10-27 23:41:16 +0200

    removed unused code

--- js/watchlater.js
+++ js/watchlater.js
@@ -1,16 +1,6 @@
 $(document).ready(function(){
   
   
-    // HAMBURGER MENU //
-    function myFunction() {
-          var x = document.getElementById("myLinks");
-          if (x.style.display === "block") {
-            x.style.display = "none";
-          } else {
-            x.style.display = "block";
-          }
-       };
-
 
 
 
