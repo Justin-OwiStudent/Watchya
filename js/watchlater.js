565b0249b10285a57fde80bf9559b372a38ce462
Liam Wedge <21100218@virtualwindow.co.za>
2021-10-29 12:58:40 +0200

    Update watchlater.js

--- js/watchlater.js
+++ js/watchlater.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
+  
+// getting the image
+    // var movie = localStorage.getItem('data-movie-id');
+    // var div = $("#movie-cards");
+    
+    // console.log(movie);
 
+    
+
+    // if(data-id === 580489 ){
+      
+    //   var img = document.createElement('img');
+
+    //   img.src = 'https://movieweb.com/movie/venom-2/';
+    //   document.getElementById("movie-cards").appendChild(img);
+    //   }
+
+
+
+
+
+    //     //       watchlist.js >
+
+    //     // 1. Get the movie id from local storage
+
+    //     // 2. perform the API call to the above link using the movie id that you stored
+
+    //     // 3. append (just like you did in the home page)
+
+
+
+
+
+    // // HAMBURGER MENU //
+    // function myFunction() {
+    //       var x = document.getElementById("myLinks");
+    //       if (x.style.display === "block") {
+    //         x.style.display = "none";
+    //       } else {
+    //         x.style.display = "block";
+    //       }
+    //    };
     
 
 
